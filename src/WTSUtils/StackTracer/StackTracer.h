3071adf6da083ec28b01a9cc37cf06b1fe1e8ec5
Honglin Ji <honglinji.cn@gmail.com>
2023-04-17 22:23:38 +0800

    1. Linux Ubuntu build OK

--- src/WTSUtils/StackTracer/StackTracer.h
+++ src/WTSUtils/StackTracer/StackTracer.h
@@ -4,7 +4,7 @@
  *
  * \author Wesley
  * \date 2021/04/13
- * 
+ *
  * \brief 堆栈查看器
  */
 #pragma once
@@ -18,6 +18,7 @@
 #endif // _WINDOWS
 
 #include <functional>
+#include <cstdlib>
 
 typedef std::function<void(const char*)> TracerLogCallback;
 
